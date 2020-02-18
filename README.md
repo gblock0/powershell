13ae0636bf05b8497a30044e5773f54e4eb05a47
Greg block <gblock@pathmatics.com>
2020-02-17 16:52:38 -0800

    Add updated git settings

---
+++ README.md
@@ -0,0 +1,26 @@
+# My Powershell profile settings:
+
+## Install
+
+- [Get-ChildItemColor](https://github.com/joonro/Get-ChildItemColor)
+  - Makes `ls` in powershell behave more similar to unix `ls`
+- [PostGit v1.X](https://github.com/dahlbyk/posh-git)
+  - ![Example Prompt](https://github.com/gblock0/powershell/screenshots/gitPrmpt)
+- [PSReadLine](https://github.com/PowerShell/PSReadLine)
+  > A bash inspired readline implementation for PowerShell
+  >
+  > This module replaces the command line editing experience of PowerShell for versions 3 and up. It provides:
+  >
+  > - Syntax coloring
+  > - Simple syntax error notification
+  > - A good multi-line experience (both editing and history)
+  > - Customizable key bindings
+  > - Cmd and emacs modes (neither are fully implemented yet, but both are usable)
+  > - Many configuration options
+  > - Bash style completion (optional in Cmd mode, default in Emacs mode)
+  > - Bash/zsh style interactive history search (CTRL-R)
+  > - Emacs yank/kill ring
+  > - PowerShell token based "word" movement and kill
+  > - Undo/redo
+  > - Automatic saving of history, including sharing history across live sessions
+  > - "Menu" completion (somewhat like Intellisense, select completion with arrows) via Ctrl+Space
---
+++ powershellSettings.ps1
@@ -0,0 +1,55 @@
+﻿# Get Git niceness in PS
+Import-Module -Name posh-git
+
+# Make ls work more like unix ls
+Import-Module Get-ChildItemColor
+Set-Alias l Get-ChildItemColor -option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+
+# Alias git to g, because it takes to long to type out git
+Set-Alias g git
+
+# **** PoshGit Prompt Settings Start *****
+
+# Show time before Path and VCS info
+$GitPromptSettings.DefaultPromptPrefix.Text = '$(Get-Date -f "MM-dd HH:mm:ss") '
+$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Magenta
+$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+
+# Use a green checkmark to show that the local repo isn't dirty
+$GitPromptSettings.LocalDefaultStatusSymbol.Text = "✔"
+$GitPromptSettings.LocalDefaultStatusSymbol.ForegroundColor = [ConsoleColor]::DarkGreen
+
+# Turn off showing that the local branch is identical to the remote
+$GitPromptSettings.BranchIdenticalStatusSymbol.Text = ""
+
+# Change the default symbol that shows the local is ahead of remote
+$GitPromptSettings.BranchAheadStatusSymbol.Text = "☁"
+$GitPromptSettings.BranchIdenticalStatusSymbol.ForegroundColor = [ConsoleColor]::Magenta
+
+# Don't show statuses when they are zero
+$GitPromptSettings.ShowStatusWhenZero = $false
+
+# Change the default dirty symbol
+$GitPromptSettings.LocalWorkingStatusSymbol.Text = "☢"
+
+# **** PoshGit Prompt Settings End *****
+
+# Import PSReadLine
+Import-Module PSReadLine
+
+# **** PSReadLine Settings Start *****
+# History Options
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
+Set-PSReadLineOption -MaximumHistoryCount 4000
+
+# History substring search
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Tab completion
+Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+# **** PSReadLine Settings End *****
